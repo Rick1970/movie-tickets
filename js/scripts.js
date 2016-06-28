af7e1f55a6a395502ef7b7337d42813498c70028
Joel Waage and Rick Thornbrugh <joel+rick@>
2016-06-28 10:34:10 -0700

    intake variables from HTML into JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
-var eve = {
-  time: ["11:00 a.m", "1:30 p.m.", "04:30 p.m.", "07:15 p.m."],
-  price: [5, 5, 5, 8],
-};
+// var eve = {
+//   time: ["11:00 a.m", "1:30 p.m.", "04:30 p.m.", "07:15 p.m."],
+//   price: [5, 5, 5, 8],
+//   rating
+// };
 
+// function Movie(title, age) {
+//   this.movieTitle = title;
+//   this.movieAge = age;
+//
+// }
 
 
 
 
 $(document).ready(function() {
+  $("form#blanks").submit(function(event) {
+  event.preventDefault();
 
+  var inputtedTitle = $("#movieTitle").val();
+  var inputtedAge = parseInt($("input#age").val());
+  alert(inputtedTitle);
+  alert(inputtedAge);
+
+  });
 });
